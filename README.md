1732abd79acf46f1649fc49cb8205b8df6375f2f
Beatstamosi <moritz.lued@gmail.com>
2025-06-06 14:02:57 -0600

    Setup project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-message-board
+
+My take on https://www.theodinproject.com/lessons/node-path-nodejs-mini-message-board.
---
+++ app.js
@@ -0,0 +1,19 @@
+import path from "node:path";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
